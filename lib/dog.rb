02f75714270b3b1c04d82598dd0a4a6336706e98
Austin Johnson <technicallyaustin@gmail.com>
2020-01-07 01:13:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,25 @@
 class Dog 
   def initialize(breed, name)
-    attr_accessor :breed, :name 
+    @breed = breed
+    @name = name
+  end
+  def breed= (breed)
+    @breed = breed
     if breed == nil 
       breed = "Mutt"
     end
+    
   end
+  def breed
+    @breed
+  end
+  
+  def name= (name)
+    @name = name
+  end
+  
+  def name 
+    @name
+  end
+  
 end
